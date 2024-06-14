4d1273a418088b90679f65ff3016848367d76e71
Siddhant jha <40361603+siddhantJH@users.noreply.github.com>
2024-06-14 08:26:54 +0530

    Setting up basic structure of the files and folders

---
+++ Internation Affair Website/src/components/Layout.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import {Header,Footer} from './index.js'
+import { Outlet } from 'react-router-dom'
+
+
+function Layout()
+{
+    return(
+        <>
+            <Header/>
+            <Outlet/>
+            <Footer/>
+        </>
+    )
+}
