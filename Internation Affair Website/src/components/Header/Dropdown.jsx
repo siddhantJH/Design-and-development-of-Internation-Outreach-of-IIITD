9f98e867f9f69a745bc188489b7e8bddb2855b48
Siddhant jha <40361603+siddhantJH@users.noreply.github.com>
2024-06-14 18:56:04 +0530

    header update

---
+++ Internation Affair Website/src/components/Header/Dropdown.jsx
@@ -0,0 +1,327 @@
+// DropdownContent.js
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
+
+
+export const HomeDropDown = ({ isOpen }) => {
+  return (
+    isOpen && (
+      <div
+        className="origin-top-right absolute right-0  mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none"
+        role="menu"
+        aria-orientation="vertical"
+        aria-labelledby="options-menu"
+      >
+        <div className="py-1" role="none">
+            
+          <a
+            href="#"
+            className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900"
+            role="menuitem"
+          >
+            Option 1
+          </a>
+          <a
+            href="#"
+            className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900"
+            role="menuitem"
+          >
+            Option 2
+          </a>
+          <a
+            href="#"
+            className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900"
+            role="menuitem"
+          >
+            Option 3
+          </a>
+        </div>
+      </div>
+    )
+  );
+};
+
+
+
+export const  InternationalAdmissionDropdown = ({ isOpen }) => {
+    return (
+      isOpen && (
+        <div
+          className="origin-top-right absolute right-0 mt-2 w-30 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none"
+          role="menu"
+          aria-orientation="vertical"
+          aria-labelledby="options-menu"
+        >
+          <div className="py-1" role="none">
+                <NavLink to="/application-process">
+                <a
+                href="#"
+                className="block px-4 py-2 text-thin text-gray-700 hover:bg-gray-100 hover:text-gray-900"
+                role="menuitem"
+                >
+                Application Process
+                </a>
+                </NavLink>
+
+                <NavLink to="/brochure">
+                <a
+              href="#"
+              className="block px-4 py-2 text-thin text-gray-700 hover:bg-gray-100 hover:text-gray-900"
+              role="menuitem"
+            >
+              Information Brochure
+            </a>
+                </NavLink>
+            
+            <NavLink to="/FAQs">
+            <a
+              href="#"
+              className="block px-4 py-2 text-thin text-gray-700 hover:bg-gray-100 hover:text-gray-900"
+              role="menuitem"
+            >
+              FAQs
+            </a>
+            </NavLink>
+            
+          </div>
+        </div>
+      )
+    );
+  };
+
+
+  export const  ScholarshipDropdown = ({ isOpen }) => {
+    return (
+      isOpen && (
+        <div
+          className="origin-top-right absolute right-0 mt-2 w-30 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none"
+          role="menu"
+          aria-orientation="vertical"
+          aria-labelledby="options-menu"
+        >
+          <div className="py-1" role="none">
+            <a
+              href="#"
+              className="block px-4 py-2  text-thin text-gray-700 hover:bg-gray-100 hover:text-gray-900"
+              role="menuitem"
+            >
+              Option 1
+            </a>
+            <a
+              href="#"
+              className="block px-4 py-2  text-thin text-gray-700 hover:bg-gray-100 hover:text-gray-900"
+              role="menuitem"
+            >
+              Option 2
+            </a>
+            <a
+              href="#"
+              className="block px-4 py-2  text-thin text-gray-700 hover:bg-gray-100 hover:text-gray-900"
+              role="menuitem"
+            >
+              Option 3
+            </a>
+          </div>
+        </div>
+      )
+    );
+  };
+
+  export const  VisaAndImmigrationDropdown = ({ isOpen }) => {
+    return (
+      isOpen && (
+        <div
+          className="origin-top-right absolute right-0 mt-2 w-30 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none"
+          role="menu"
+          aria-orientation="vertical"
+          aria-labelledby="options-menu"
+        >
+          <div className="py-1" role="none">
+            <a
+              href="#"
+              className="block px-4 py-2  text-thin text-gray-700 hover:bg-gray-100 hover:text-gray-900"
+              role="menuitem"
+            >
+              Option 1
+            </a>
+            <a
+              href="#"
+              className="block px-4 py-2  text-thin text-gray-700 hover:bg-gray-100 hover:text-gray-900"
+              role="menuitem"
+            >
+              Option 2
+            </a>
+            <a
+              href="#"
+              className="block px-4 py-2  text-thin text-gray-700 hover:bg-gray-100 hover:text-gray-900"
+              role="menuitem"
+            >
+              Option 3
+            </a>
+          </div>
+        </div>
+      )
+    );
+  };
+
+
+  export const  StudentsDropdown = ({ isOpen }) => {
+    return (
+      isOpen && (
+        <div
+          className="origin-top-right absolute right-0 mt-2 w-40 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none"
+          role="menu"
+          aria-orientation="vertical"
+          aria-labelledby="options-menu"
+        >
+          <div className="py-1" role="none">
+            <NavLink to="/testimony">
+            <a
+              href="#"
+              className="block px-4 py-2  text-thin text-gray-700 hover:bg-gray-100 hover:text-gray-900"
+              role="menuitem"
+            >
+              International Student Testimony
+            </a>
+            </NavLink>
+            <NavLink to="student-support">
+            <a
+              href="#"
+              className="block px-4 py-2  text-thin text-gray-700 hover:bg-gray-100 hover:text-gray-900"
+              role="menuitem"
+            >
+              Internation Student Support
+            </a>
+
+            </NavLink>
+
+
+            <NavLink to="/blog">
+            <a
+              href="#"
+              className="block px-4 py-2  text-thin text-gray-700 hover:bg-gray-100 hover:text-gray-900"
+              role="menuitem"
+            >
+              Blog
+            </a>
+            </NavLink>
+           
+          </div>
+        </div>
+      )
+    );
+  };
+
+  export const  PartnersDropdown = ({ isOpen }) => {
+    return (
+      isOpen && (
+        <div
+          className="origin-top-right absolute right-0 mt-2 w-40 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none"
+          role="menu"
+          aria-orientation="vertical"
+          aria-labelledby="options-menu"
+        >
+          <div className="py-1" role="none">
+            <NavLink to="/joint-program">
+            <a
+              href="#"
+              className="block px-4 py-2  text-thin text-gray-700 hover:bg-gray-100 hover:text-gray-900"
+              role="menuitem"
+            >
+              Join Degree Program
+            </a>
+            </NavLink>
+
+            <NavLink to="/summer-exchange">
+            <a
+              href="#"
+              className="block px-4 py-2  text-thin text-gray-700 hover:bg-gray-100 hover:text-gray-900"
+              role="menuitem"
+            >
+              Summer Exchange 
+            </a>
+            </NavLink>
+            
+            <NavLink to="/summer-program">
+            <a
+              href="#"
+              className="block px-4 py-2  text-thin text-gray-700 hover:bg-gray-100 hover:text-gray-900"
+              role="menuitem"
+            >
+              Summer Program
+            </a>
+            </NavLink>
+            
+            <NavLink to="/winter-program">
+            <a
+              href="#"
+              className="block px-4 py-2  text-thin text-gray-700 hover:bg-gray-100 hover:text-gray-900"
+              role="menuitem"
+            >
+              Winter Program
+            </a>
+            </NavLink>
+            
+            <NavLink to="/faculty-development">
+            <a
+              href="#"
+              className="block px-4 py-2  text-thin text-gray-700 hover:bg-gray-100 hover:text-gray-900"
+              role="menuitem"
+            >
+              Faculty Development Program
+            </a>
+            </NavLink>
+           
+          </div>
+        </div>
+      )
+    );
+  };
+
+  export const  CampusDropdown = ({ isOpen }) => {
+    return (
+      isOpen && (
+        <div
+          className="origin-top-right absolute right-0 mt-2 w-40 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none"
+          role="menu"
+          aria-orientation="vertical"
+          aria-labelledby="options-menu"
+        >
+          <div className="py-1" role="none">
+            <NavLink to="/virtual-tour">
+            <a
+              href="#"
+              className="block px-4 py-2  text-thin text-gray-700 hover:bg-gray-100 hover:text-gray-900"
+              role="menuitem"
+            >
+             Virtual Campus Tour            </a>
+            </NavLink>
+            
+            <NavLink to="/facilities">
+            <a
+              href="#"
+              className="block px-4 py-2  text-thintext-gray-700 hover:bg-gray-100 hover:text-gray-900"
+              role="menuitem"
+            >
+              Hostell and Mess Facilities
+            </a>
+            </NavLink>
+           
+           <NavLink to="wellbeing">
+           <a
+              href="#"
+              className="block px-4 py-2 text-thin text-gray-700 hover:bg-gray-100 hover:text-gray-900"
+              role="menuitem"
+            >
+              Wellbeing Cell
+            </a>
+           </NavLink>
+           
+          </div>
+        </div>
+      )
+    );
+  };
+
+export default {HomeDropDown,InternationalAdmissionDropdown,ScholarshipDropdown,VisaAndImmigrationDropdown,StudentsDropdown,PartnersDropdown,CampusDropdown}
--- Internation Affair Website/src/components/Header/Header.jsx
+++ Internation Affair Website/src/components/Header/Header.jsx
@@ -1,11 +1,57 @@
-import React from "react";
+import { useState } from "react";
+import { GrClose } from "react-icons/gr";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { HomeDropDown,InternationalAdmissionDropdown,ScholarshipDropdown,VisaAndImmigrationDropdown,StudentsDropdown,PartnersDropdown,CampusDropdown } from "./Dropdown";
 
-function Header()
-{
-    return(<>
-    <h1>This is header</h1>
-    </>)
+function Header() {
+    const [showMenu,setShowMenu]=useState('');
+    const [homeOpen, sethomeOpen] = useState(false);
+    const [intOpen, setintOpen] = useState(false);
+    const [schOpen, setschOpen] = useState(false);
+    const [visaOpen, setvisaOpen] = useState(false);
+    const [partnersOpen, setpartnersOpen] = useState(false);
+    const [campusOpen, setcampusOpen] = useState(false);
+    const [studOpen, setstudOpen] = useState(false);
+  return (
+    <header className="border-b-2 border-cyan-300 flex flex-row items-center shadow-md justify-end sm:justify-around  border-b-2 bg-white">
+      <a
+        href="/"
+        className=""
+      >
+        <img src="https://www.iiitd.ac.in/sites/default/files/images/logo/logo.jpg" className="w-30 h-10" alt="" />
+      </a>
+      <nav className="border-cyan-500 text-sm text-xs hidden sm:flex flex-end justify-between items-center gap-4 font-semibold">
+        <h6 href="#" onMouseEnter={() => sethomeOpen(false)} onMouseLeave={() => sethomeOpen(false)} className=" font-normal hover:shadow-xl  relative text-xs z-10 bg-gray-white py-2 px-2  transition-all duration-300 ">
+          Home
+          <HomeDropDown isOpen={homeOpen}/>
+        </h6>
+        <h6 href="#"  onMouseEnter={() => setintOpen(true)} onMouseLeave={() => setintOpen(false)} className="font-normal hover:shadow-xl  text-xs relative z-10 bg-gray-white py-2 px-2  transition-all duration-300" >
+          International Admission
+          <InternationalAdmissionDropdown isOpen={intOpen}/>
+        </h6>
+        <h6 href="#"  onMouseEnter={() => setschOpen(false)} onMouseLeave={() => setschOpen(false)} className="font-normal hover:shadow-xl  text-xs relative z-10 bg-gray-white py-2 px-2  transition-all duration-300" >
+         Scholarship
+         <ScholarshipDropdown isOpen={schOpen}/>
+        </h6>
+        <h6 href="#" onMouseEnter={() =>setvisaOpen(false)} onMouseLeave={() => setvisaOpen(false)} className="font-normal hover:shadow-xl  relative text-xs z-10 bg-gray-white py-2 px-2  transition-all duration-300">
+         Visa and Immigration
+         <VisaAndImmigrationDropdown isOpen={visaOpen}/>
+        </h6>
+        <h6 href="#"  onMouseEnter={() => setstudOpen(true)} onMouseLeave={() => setstudOpen(false)} className="font-normal hover:shadow-xl text-xs relative z-10 bg-gray-white py-2 px-2  transition-all duration-300" >
+        Students
+        <StudentsDropdown isOpen={studOpen}/>
+        </h6>
+        <h6 href="#"  onMouseEnter={() => setpartnersOpen(true)} onMouseLeave={() => setpartnersOpen(false)} className="font-normal hover:shadow-xl  text-xs relative z-10 bg-gray-white py-2 px-2  transition-all duration-300" >
+           Partners
+           <PartnersDropdown isOpen={partnersOpen}/>
+        </h6>
+        <h6 href="#"  onMouseEnter={() => setcampusOpen(true)} onMouseLeave={() => setcampusOpen(false)} className="font-normal hover:shadow-xl  text-xs relative z-10 bg-gray-white py-2 px-2  transition-all duration-300" >
+          Campus
+          <CampusDropdown isOpen={campusOpen}/>
+        </h6>
+      </nav>
+    </header>
+  );
 }
 
-
-export default Header
+export default Header;
